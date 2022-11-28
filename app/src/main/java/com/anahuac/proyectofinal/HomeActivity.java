9c26b4e711cd50840735e0a5a02325ae943b0c3b
Adiel <ruben.luna86@anahuac.mx>
2022-11-27 21:29:19 -0800

    Home Screen

--- app/src/main/java/com/anahuac/proyectofinal/HomeActivity.java
+++ app/src/main/java/com/anahuac/proyectofinal/HomeActivity.java
@@ -5,16 +5,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.anahuac.proyectofinal.calendar.CalendarActivity;
 import com.anahuac.proyectofinal.todolist.TodoList;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class HomeActivity extends AppCompatActivity {
+
+    TextView userName;
+    private FirebaseAuth mAuth;
+    private FirebaseUser user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        userName = findViewById(R.id.userName);
+        mAuth = FirebaseAuth.getInstance();
+        user = mAuth.getCurrentUser();
+        userName.setText("¡Bienvenido "+user.getDisplayName()+"!");
     }
 
     public void openCalendar(View view){
